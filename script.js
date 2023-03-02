0fe53d78a772baf13368ad1b839a599f9ac38838
Jack Sebben <jacksebben11@hotmail.com>
2023-03-02 08:07:54 -0500

    Added the subtract, multiply and divide functions. They all use rest parameters to take an underdetermined amount of numbers to perform calculations on. Unlike the add function, they take the first argument passed as a starting point.

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
  // OPERATOR FUNCTIONS
 
- function add(...numbers){
+ function add(...numbers){ // ADD
     let sum = 0 ;
     for (const number of numbers){
         sum += number
     }
     return sum
  }
+
+ function subtract(a, ...numbers){ // SUBTRACT
+    let sub = a;
+    for(const number of numbers){
+        sub -= number
+    }
+    return sub
+ }
+
+ function multiply(a, ...numbers){ // MULTIPLY
+    let multi = a;
+    for(const number of numbers){
+        multi *= number
+    }
+    return multi
+ }
+
+ function divide(a, ...numbers){ // DIVIDE
+    let divi = a;
+    for(const number of numbers){
+        divi /= number
+    }
+    return divi
+ }
+
+
+
+
 
